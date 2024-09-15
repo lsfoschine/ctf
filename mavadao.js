03e9606f58ab15e80d25f7209b57d7d2eb061cfd
Fuska_ <47157254+lsfoschine@users.noreply.github.com>
2024-09-15 17:23:01 -0300

    Update mavadao.js

--- mavadao.js
+++ mavadao.js
@@ -1,2 +1,10 @@
 const fs = require('fs');
-console.log(fs.readFileSync('flag.txt', 'utf8'));
+
+// Ler o conteúdo do arquivo flag.txt e imprimir
+fs.readFile('flag.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Erro ao ler o arquivo:', err);
+    return;
+  }
+  console.log(data);
+});
